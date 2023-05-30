fbb48056fd7fc0919d5169b8c9744514ba89785a
M-519 <101125419+M-519@users.noreply.github.com>
2023-05-30 19:41:46 +0300

    add func playRound

--- gameScript.js
+++ gameScript.js
@@ -12,7 +12,7 @@ function getPlayerChoice() {
     return playerSelection;
 }
 
-function playRound() {
+function playRound(a, b) {
     var pcChoice = getComputerChoice();
     console.log(`Hint, the computer chose ${pcChoice}`);
 
@@ -21,23 +21,40 @@ function playRound() {
     if (playerSelection == pcChoice) {console.log("Draw")}
     else {
         if (playerSelection == "rock" && pcChoice == "paper") 
-        {console.log("Player loses, due to Rock vs Paper")}
+        {console.log("Player loses, due to Rock vs Paper");
+        b++;}
         
         if (playerSelection == "rock" && pcChoice == "scissors") 
-        {console.log("Player wins, due to Rock vs Scissors")}
+        {console.log("Player wins, due to Rock vs Scissors");
+        a++;}
     
         if (playerSelection == "paper" && pcChoice == "rock") 
-        {console.log("Player wins, due to Paper vs Rock")}
+        {console.log("Player wins, due to Paper vs Rock");
+        a++;}
 
         if (playerSelection == "paper" && pcChoice == "scissors") 
-        {console.log("Player loses, due to Paper vs Scissors")}
+        {console.log("Player loses, due to Paper vs Scissors");
+        b++;}
 
         if (playerSelection == "scissors" && pcChoice == "rock") 
-        {console.log("Player loses, due to Scissors vs Rock")}
+        {console.log("Player loses, due to Scissors vs Rock");
+        b++;}
 
         if (playerSelection == "scissors" && pcChoice == "paper") 
-        {console.log("Player wins, due to Scissors vs Paper")}
+        {console.log("Player wins, due to Scissors vs Paper");
+        a++;}
     }
 }
 
-getGameResult();
+// add loop
+var pcWins = 0, playerWins = 0;
+playRound(pcWins, playerWins);
+playRound(pcWins, playerWins);
+playRound(pcWins, playerWins);
+playRound(pcWins, playerWins);
+playRound(pcWins, playerWins);
+
+console.log(`Result: ${pcWins} for the machine, to ${playerWins} for the player`)
+    if (pcWins > playerWins) {console.log("The machine wins")}
+    else if (pcWins < playerWins) {console.log("The player wins")}
+    else {console.log("It's a draw!")}

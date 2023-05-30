e0048c7735c6b85cc3264496fe202de86c7d376c
M-519 <101125419+M-519@users.noreply.github.com>
2023-05-30 18:29:31 +0300

    add pc rand func

--- gameScript.js
+++ gameScript.js
@@ -1,1 +1,20 @@
-console.log("text");
+console.log("test");
+
+
+
+function getComputerChoice () {
+    var pcChoice =  (Math.floor(Math.random()*3)+1);
+    if (pcChoice === 1) {return "Rock"};
+    if (pcChoice === 2) {return "Paper"};
+    if (pcChoice === 3) {return "Scissors"};
+} 
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
